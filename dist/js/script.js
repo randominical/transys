1fae61e40981cea4f419a725ecbc03e3577d1e87
Anna Agafonova <aa.agafonova@gmail.com>
2020-11-12 23:29:16 +0300

    Попытка стилизовать карту через https://mapstyle.withgoogle.com/
    Неудачно

--- dist/js/script.js
+++ dist/js/script.js
@@ -29,16 +29,34 @@ $(document).ready(function(){
   });
 });;
 window.initMap = function() {
-    var location = {lat: 51.47672559, lng: -3.17107379};
-    var markerloc = {lat: 51.476852, lng: -3.167869};
+    var location = {lat: 52.090185, lng: 23.694595};
+    var markerloc = {lat: 52.090185, lng: 23.694595};
     map = new google.maps.Map(document.getElementById('map'), {
        center: location,
        scrollwheel: false,
-       zoom: 17
+       zoom: 16
     });
     var marker = new google.maps.Marker({
        position: markerloc,
        map: map,
-       title: 'Hello World!'
     });
+    styles: [
+        {
+          "elementType": "geometry.fill",
+          "stylers": [
+            {
+              "color": "#004399"
+            },
+            {
+              "saturation": 5
+            },
+            {
+              "lightness": 65
+            },
+            {
+              "weight": 2.5
+            }
+          ]
+        }
+      ]
   };;
