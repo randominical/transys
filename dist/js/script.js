f0de509b39f2d82fef8a68241ae5448bdf1d7d56
Anna Agafonova <aa.agafonova@gmail.com>
2020-11-15 22:40:53 +0300

    Усовершенствование меню

--- dist/js/script.js
+++ dist/js/script.js
@@ -81,7 +81,7 @@ $(document).ready(function() {
 });
 
 $(document).ready(function() {
-    $('.menu__list').click(function(event) {
+    $('.menu__list, .body-menu__logo').click(function(event) {
         $('.menu__burger, .menu__body, .body-menu__logo, .body-menu__bottom').removeClass('active');
         $('body').removeClass('lock');
     });
