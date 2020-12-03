5e278829f9f58329fb4936aae2e9e0bf935362e5
randominical <aa.agafonova@gmail.com>
2020-12-03 23:05:54 +0300

    отката бага меню

--- #src/js/menu.js
+++ #src/js/menu.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $('.menu__burger').click(function(event) {
-        $('.menu__burger, .menu__body, .menu__cover, .body-menu__logo, .body-menu__bottom').toggleClass('active');
+        $('.menu__burger, .menu__body, .body-menu__logo, .body-menu__bottom').toggleClass('active');
         $('body').toggleClass('lock');
     });
 });
@@ -8,6 +8,6 @@ $(document).ready(function() {
 $(document).ready(function() {
     $('.body-menu__link, .body-menu__logo').click(function(event) {
         $('body').removeClass('lock');
-        $('.menu__burger, .menu__body, .menu__cover, .body-menu__logo, .body-menu__bottom').removeClass('active');
+        $('.menu__burger, .menu__body, .body-menu__logo, .body-menu__bottom').removeClass('active');
     });
 });
